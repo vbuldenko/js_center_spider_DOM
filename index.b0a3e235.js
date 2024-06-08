392fbeb1f82087ad7f7182b7e1e928b9c2b9a18a
Vladyslav <vladbuldenko@gmail.com>
2024-06-08 19:06:43 +0300

    make build

---
+++ index.b0a3e235.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top="".concat((e.clientHeight-t.clientHeight)/2,"px"),t.style.left="".concat((e.clientWidth-t.clientWidth)/2,"px");
+//# sourceMappingURL=index.b0a3e235.js.map
